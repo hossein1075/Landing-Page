3ac63403d1384a736e4bd8029dca2791df7d02d4
Hossein Hosseini <hosseinh1075@gmail.com>
2025-06-11 14:14:57 +0430

    Initial commit

---
+++ script/app.js
@@ -0,0 +1,86 @@
+"use strict"
+let $ = document
+
+// let navbarBtn =$.getElementById('btn-navbar')
+// let navRemoveBtn =$.getElementById('nav-remove')
+// let navToggleBtn =$.querySelector('.nav_toggle')
+let nextBtn = $.getElementById('next');
+let preBtn = $.getElementById('pre');
+let slidItem = $.querySelectorAll('.main_about_portfolio');
+let slider = $.querySelector('.main_about_slider');
+
+let currentIndex = 0;
+let autoContect;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navbarBtn = document.querySelector("#btn-navbar");
+    const navRemoveBtn = document.querySelector("#nav-remove");
+    const navToggle = document.querySelector(".nav_toggle");
+  
+    navbarBtn.addEventListener("click", () => {
+      navToggle.classList.add("nav_toggle-active");
+    });
+  
+    navRemoveBtn.addEventListener("click", () => {
+      navToggle.classList.remove("nav_toggle-active");
+    });
+  });
+
+function showItem(index, move) {
+
+    slidItem.forEach((slide) => {
+        slide.classList.remove('main_about_portfolio_left--Active', 'slide-in-right', 'slide-in-left', 'slide-out-left', 'slide-out-right');
+    });
+
+    if (move === 'next') {
+        slidItem[index].classList.add('slide-in-right');
+        if (slidItem[currentIndex]) {
+            slidItem[currentIndex].classList.add('slide-out-left');
+        }
+    } else if (move === 'pre') {
+        slidItem[index].classList.add('slide-in-left');
+        if (slidItem[currentIndex]) {
+            slidItem[currentIndex].classList.add('slide-out-right');
+        }
+    } else {
+
+        slidItem[index].classList.add('main_about_portfolio_left--Active');
+    }
+
+    currentIndex = index;
+
+
+    setTimeout(() => {
+        slidItem.forEach(slide => {
+            slide.classList.remove('slide-in-right', 'slide-in-left', 'slide-out-left', 'slide-out-right');
+        });
+        slidItem[currentIndex].classList.add('main_about_portfolio_left--Active');
+    }, 500);
+}
+
+function autoSlide() {
+    clearInterval(autoContect)
+     autoContect = setInterval(() => {
+        let newIndex = (currentIndex + 1) % slidItem.length;
+        showItem(newIndex, 'next')
+    }, 3000);
+
+}
+
+nextBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let newIndex = (currentIndex + 1) % slidItem.length;
+    showItem(newIndex, 'next');
+    autoSlide()
+});
+
+preBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let newIndex = (currentIndex - 1 + slidItem.length) % slidItem.length;
+    showItem(newIndex, 'pre');
+    autoSlide()
+});
+
+
+showItem(currentIndex);
+autoSlide()
