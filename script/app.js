3ffdcac3fd98b79eec96f2df2961cadc609ace3d
Hossein Hosseini <hosseinh1075@gmail.com>
2025-06-18 14:32:27 +0430

    footer & scroll

--- script/app.js
+++ script/app.js
@@ -84,3 +84,14 @@ preBtn.addEventListener('click', (e) => {
 
 showItem(currentIndex);
 autoSlide()
+
+document.querySelectorAll('.nav_dropdown_link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').slice(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  });
